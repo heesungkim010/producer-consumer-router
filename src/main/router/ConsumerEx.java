226be847a6b22436053eecf91e38dccb755853dd
heesungkim010 <heesungkim010@gmail.com>
2022-02-15 10:49:37 +0900

    prototype using restricted size buffer

--- src/main/router/ConsumerEx.java
+++ src/main/router/ConsumerEx.java
@@ -35,7 +35,7 @@ public class ConsumerEx implements Runnable {
                 e.printStackTrace();
             }
             try {
-                Thread.sleep(100);
+                Thread.sleep(200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
